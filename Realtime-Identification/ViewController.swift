cfaa2548d172c37484c0d107c97c689a6f79068d
AnimatorJoe <josephjin7@gmail.com>
2017-10-08 19:47:28 -0500

    Initial Commit

---
+++ Realtime-Identification/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Realtime-Identification
+//
+//  Created by Joseph Jin on 10/8/17.
+//  Copyright © 2017 AnimatorJoe. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Starting up the camera
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
